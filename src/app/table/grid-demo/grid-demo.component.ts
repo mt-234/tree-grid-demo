e32c04ef1281d998d4126f21cf002d96ad0530fd
Gaurav Prajapati <mt_063@outlook.com>
2021-07-21 15:25:43 +0530

    implement multiple row and cells selctio

--- src/app/table/grid-demo/grid-demo.component.ts
+++ src/app/table/grid-demo/grid-demo.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { CheckBoxComponent } from '@syncfusion/ej2-angular-buttons';
-import { DropDownListComponent, ChangeEventArgs } from '@syncfusion/ej2-angular-dropdowns';
+import {
+  DropDownListComponent,
+  ChangeEventArgs,
+} from '@syncfusion/ej2-angular-dropdowns';
 import {
   PageService,
   SortService,
@@ -35,20 +38,40 @@ export class GridDemoComponent implements OnInit {
   @ViewChild('price')
   public price: CheckBoxComponent;
   constructor() {}
-  /* Start for sorting*/
+  /* End for sorting*/
 
-  /*start for filter*/
+  /*Start for filter*/
   public pageSettings: Object;
   public filterSettings: Object;
   public d1data: Object;
   public ddlfields: Object;
   public d2data: Object;
   public typefields: Object;
-  
+
   @ViewChild('dropdown1')
   public dropdown1: DropDownListComponent;
   @ViewChild('dropdown2')
   public dropdown2: DropDownListComponent;
+  /* End for filter*/
+
+  /* Start for selection */
+  public selectionSettings: Object;
+  public selectd1data: Object;
+  public fields1: Object;
+  public selectd2data: Object;
+  public fields2: Object;
+  public selectd3data: Object;
+  public fields3: Object;
+  @ViewChild('selectdropdown1')
+  public selectdropdown1: DropDownListComponent;
+
+  @ViewChild('selectdropdown2')
+  public selectdropdown2: DropDownListComponent;
+
+  @ViewChild('selectdropdown3')
+  public selectdropdown3: DropDownListComponent;
+  /* End for selection*/
+
 
   ngOnInit(): void {
     this.data = sortData;
@@ -61,7 +84,7 @@ export class GridDemoComponent implements OnInit {
     };
 
     // for filtering
-    this.pageSettings = { pageSize: 10 };
+    this.pageSettings = { pageSize: 10, pageSizes: true };
     this.filterSettings = { type: 'Menu', hierarchyMode: 'Parent' };
     this.ddlfields = { text: 'mode', value: 'id' };
     this.typefields = { text: 'mode', value: 'id' };
@@ -74,6 +97,24 @@ export class GridDemoComponent implements OnInit {
     this.d2data = [
       { id: 'Menu', mode: 'Menu' },
       { id: 'Excel', mode: 'Excel' },
+    ];
+
+    // for selection
+    this.selectionSettings = { type: 'Multiple' };
+    this.fields1 = { text: 'type', value: 'id' };
+    (this.selectd1data = [
+      { id: 'Single', type: 'Single' },
+      { id: 'Multiple', type: 'Multiple' },
+    ]),
+      (this.fields2 = { text: 'mode', value: 'id' });
+    (this.selectd2data = [
+      { id: 'Row', mode: 'Row' },
+      { id: 'Cell', mode: 'Cell' },
+    ]),
+      (this.fields3 = { text: 'mode', value: 'id' });
+    this.selectd3data = [
+      { id: 'Flow', mode: 'Flow' },
+      { id: 'Box', mode: 'Box' },
     ];
   }
 
@@ -155,4 +196,31 @@ export class GridDemoComponent implements OnInit {
     this.treegrid.clearFiltering();
   }
   /* End for filter*/
+
+  /* Start for selection*/
+  change1(e: ChangeEventArgs): void {
+    let type: any = <string>e.value;
+    let mode: any = <string>this.selectdropdown2.value;
+    this.treegrid.selectionSettings.type = type;
+    if (type === 'Multiple' && mode === 'Cell') {
+      document.getElementById('cellselection').style.display = 'table-row';
+    } else {
+      document.getElementById('cellselection').style.display = 'none';
+    }
+  }
+  change2(e: ChangeEventArgs): void {
+    let mode: any = e.value;
+    let type: any = <string>this.selectdropdown1.value;
+    this.treegrid.selectionSettings.mode = mode;
+    if (type === 'Multiple' && mode === 'Cell') {
+      document.getElementById('cellselection').style.display = 'table-row';
+    } else {
+      document.getElementById('cellselection').style.display = 'none';
+    }
+  }
+  change3(e: ChangeEventArgs): void {
+    let cellmode: any = <string>e.value;
+    this.treegrid.selectionSettings.cellSelectionMode = cellmode;
+  }
+   /* End for selection*/
 }
